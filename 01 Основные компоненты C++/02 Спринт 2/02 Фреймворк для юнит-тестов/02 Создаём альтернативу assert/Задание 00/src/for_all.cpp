553a84f6ae09feb98d6f36f9fe86bef54211ec3a
TheEvilMandarin <nastyag1608@gmail.com>
2023-03-18 23:57:51 +0300

    From sprint 2->02->02 to 2->02->08

---
+++ 01 Основные компоненты C++/02 Спринт 2/02 Фреймворк для юнит-тестов/02 Создаём альтернативу assert/Задание 00/src/for_all.cpp
@@ -0,0 +1,24 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Assert(bool value, const string& hint) {
+    // Реализуйте тело функции Assert
+    if (value == false) {
+        cout << "Assertion failed: "s;
+        if (!hint.empty()) {
+            cout << "Hint: "s << hint;
+        }
+        abort();
+    }
+}
+
+int main() {
+    const string greeting = "Hello"s;
+    // Намеренная ошибка в условии, чтобы показать работу Assert
+    Assert(greeting.empty(), "Greeting must be non-empty"s);
+    // Следующая строка не должна выполниться, так как Assert аварийно завершит работу программы
+    cout << "This line will not be printed"s << endl;
+}
---
+++ 01 Основные компоненты C++/02 Спринт 2/02 Фреймворк для юнит-тестов/05 Применяем фреймворк на практике/Задание 00/src/for_all.cpp
@@ -0,0 +1,146 @@
+﻿#include <cassert>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+template <typename T1, typename T2>
+ostream& operator<<(ostream& out, const pair<T1, T2>& container) {
+    out << container.first << ": " << container.second;
+    return out;
+}
+
+template <typename Documents>
+ostream& Print(ostream& out, Documents container) {
+    for (auto element = container.begin(); element != prev(container.end()); element++) {
+        out << *element << ", "s;
+    }
+    out << *container.rbegin();
+    return out;
+}
+
+template <typename T1, typename T2>
+ostream& operator <<(ostream& out, const map<T1, T2>& container)
+{
+    out << '{';
+    &Print(out, container);
+    out << '}';
+    return out;
+}
+
+template <typename T, typename U>
+void AssertEqualImpl(const T& t, const U& u, const string& t_str, const string& u_str, const string& file,
+    const string& func, unsigned line, const string& hint) {
+    if (t != u) {
+        cout << boolalpha;
+        cout << file << "("s << line << "): "s << func << ": "s;
+        cout << "ASSERT_EQUAL("s << t_str << ", "s << u_str << ") failed: "s;
+        cout << t << " != "s << u << "."s;
+        if (!hint.empty()) {
+            cout << " Hint: "s << hint;
+        }
+        cout << endl;
+        abort();
+    }
+}
+
+#define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, ""s)
+
+#define ASSERT_EQUAL_HINT(a, b, hint) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, (hint))
+
+class Synonyms {
+public:
+    void Add(const string& first_word, const string& second_word) {
+        synonyms_[first_word].insert(second_word);
+        synonyms_[second_word].insert(first_word);
+    }
+
+    size_t GetSynonymCount(const string& word) const {
+        if (synonyms_.count(word) != 0) {
+            return synonyms_.at(word).size();
+        }
+        return 0;
+    }
+
+    bool AreSynonyms(const string& first_word, const string& second_word) const {
+        if (synonyms_.count(first_word) != 0) {
+            return synonyms_.at(first_word).count(second_word) != 0;
+        }
+        return false;
+    }
+
+private:
+    map<string, set<string>> synonyms_;
+};
+
+void TestAddingSynonymsIncreasesTheirCount() {
+    Synonyms synonyms;
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 0);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s), 0);
+
+    synonyms.Add("music"s, "melody"s);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s), 1);
+
+    synonyms.Add("music"s, "tune"s);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 2);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("tune"s), 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s), 1);
+}
+
+void TestAreSynonyms() {
+    Synonyms synonyms;
+    synonyms.Add("winner"s, "champion"s);
+    synonyms.Add("good"s, "nice"s);
+
+    ASSERT_EQUAL(synonyms.AreSynonyms("winner"s, "champion"s), true);
+    ASSERT_EQUAL(synonyms.AreSynonyms("champion"s, "winner"s), true);
+
+    ASSERT_EQUAL(!synonyms.AreSynonyms("good"s, "champion"s), true);
+    ASSERT_EQUAL(synonyms.AreSynonyms("good"s, "nice"s), true);
+}
+
+void TestSynonyms() {
+    TestAddingSynonymsIncreasesTheirCount();
+    TestAreSynonyms();
+}
+
+int main() {
+    TestSynonyms();
+
+    Synonyms synonyms;
+
+    string line;
+    while (getline(cin, line)) {
+        istringstream command(line);
+        string action;
+        command >> action;
+
+        if (action == "ADD"s) {
+            string first_word, second_word;
+            command >> first_word >> second_word;
+            synonyms.Add(first_word, second_word);
+        }
+        else if (action == "COUNT"s) {
+            string word;
+            command >> word;
+            cout << synonyms.GetSynonymCount(word) << endl;
+        }
+        else if (action == "CHECK"s) {
+            string first_word, second_word;
+            command >> first_word >> second_word;
+            if (synonyms.AreSynonyms(first_word, second_word)) {
+                cout << "YES"s << endl;
+            }
+            else {
+                cout << "NO"s << endl;
+            }
+        }
+        else if (action == "EXIT") {
+            break;
+        }
+    }
+}
